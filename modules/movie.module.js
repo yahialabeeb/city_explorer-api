f53f39d2310fe3b4e1dc2aab3dda378135b6b18d
Yahialabeeb <yahia.labeeb@gmail.com>
2021-09-10 10:37:39 +0300

    lab10

--- modules/movie.module.js
+++ modules/movie.module.js
@@ -1,32 +1,36 @@
-
+'use strict'
 const axios = require('axios')
 
+const Cache = require('../helpers/Cache.helper.js')
 
-
-
+let cacher = new Cache();
 
 // create the function to get data from movie api server and send it for the clint 
 function handleMovie(req, res) {
-    let cityName1 = req.query.cityName
-    let movieURL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${cityName1}`
-    let arrOfMovies = []
-    axios.get(movieURL).then(result => {
-        result.data.results.forEach((item) => {
-            // there is no img url 
-            arrOfMovies.push(new Movies(item.title, item.overview, item.vote_average, item.vote_count, item.popularity, item.release_date));
+
+    //search for city in cacher 
+
+    let cityName = req.query.cityName
+    if (cacher.datacached[cityName] === undefined) {
+// if it is not in cacher 
+        let movieURL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${cityName}`
+        let arrOfMovies = []
+        axios.get(movieURL).then(result => {
+            result.data.results.forEach((item) => {
+                // there is no img url 
+                arrOfMovies.push(new Movies(item.title, item.overview, item.vote_average, item.vote_count, item.popularity, item.release_date));
+
+                cacher.datacached[cityName] = arrOfMovies
+                console.log("not cached");
+            })
+            res.send(cacher.datacached[cityName]);
         })
-        console.log(arrOfMovies);
-    res.send(arrOfMovies);
-    })
-
-
-
-    
-
-
-
+// if it is in cacher 
+    } else {
+        console.log("not cached");
+        res.send(cacher.datacached[cityName]);
+    }
 }
-
 
 // making a class to saving data from external api server as a object
 class Movies {
@@ -40,5 +44,11 @@ class Movies {
         this.popularity = popularity;
     }
 }
-
+//  after 1 day server wil reset the data 
+const dayInMilSec = 86400000;
+  const oneDayPassed = (Date.now() - cacher.timeStamp) > dayInMilSec;
+  if (oneDayPassed) {
+      console.log('Cache Reset');
+     cacher = new Cache();
+  }
 module.exports = handleMovie;
---
+++ modules/yelp.js
@@ -0,0 +1,6 @@
+
+
+
+
+
+yelpUrl = `https://api.yelp.com/v3/businesses/search?categories=Restaurants&latitude=35.5&longitude=39`
